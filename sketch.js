88ff7cec368043539abfa2fab79e6b89ccc56809
walisonls <gwalisonls@gmail.com>
2022-06-21 12:46:05 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(500, 500);
+    background(0);
+
+    var n = new Matrix(2,2)
+    var n1 = new Matrix(2,2)
+    Matrix.add(n,n1)
+}
+
+function draw(){
+
+
+
+}
