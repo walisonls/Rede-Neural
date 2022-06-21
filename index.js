95c8dd70df65d76945fcebdcac5ab06c42e94b2d
walisonls <gwalisonls@gmail.com>
2022-06-21 16:55:01 -0300

    Enter

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.use('/public', express.static('public'))
+
+//rota do arquivo html princiapal
+app.get('/',(req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+//porta do servidor local
+const port = 5080
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
