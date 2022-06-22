ae0dfe7a11a6a561a62b285cbe8515b022d2ca95
walisonls <gwalisonls@gmail.com>
2022-06-21 22:27:08 -0300

    Enter

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-
 app.use('/public', express.static('public'))
 
 //rota do arquivo html princiapal
--- public/main.js
+++ public/main.js
@@ -1,1 +1,8 @@
-alert("ola")
+let rows = [1,5,2,4]
+let cols = [4,5,6,4]
+
+for(i=0;i<rows.length;i++){
+    for(j=0;j<cols.length;j++){
+        console.log(i,j)
+    }
+}
